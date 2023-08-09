88279dee9c3ce5cf968c9b46128eecc1424b7f25
Caíque Marcelino Souza <caique@brhost.io>
2023-08-09 13:06:56 -0300

    Fix routes

--- sistema/routes/web.php
+++ sistema/routes/web.php
@@ -4,8 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    $o= new \App\Libs\GoogleCustomSearch();
-    $searches=\App\Models\Search::where("id",5)->first();
-    dd($o->search($searches->search_term));
     return redirect('admin/login');
 });
